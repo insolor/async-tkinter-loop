f520c5259cd3db3ede648e7ede1138c6ca45dfd9
insolor <insolor@gmail.com>
2020-12-29 15:34:42 +0300

    Simulate delayed window closure

--- tests/test_updater.py
+++ tests/test_updater.py
@@ -1,15 +1,28 @@
+import asyncio
 import pytest
 from tk_mock import create_tk_mock
 
 
 @pytest.mark.timeout(0.1)
 def test_updater():
-    tk_mock = create_tk_mock()
+    root = create_tk_mock()
     
     def update():
-        tk_mock.close()
+        root.close()
     
-    tk_mock.update = update
+    root.update = update
     
-    tk_mock.mainloop()
-    # assert False
+    root.mainloop()
+
+
+@pytest.mark.timeout(0.3)
+def test_simulate_delayed_window_closure():
+    root = create_tk_mock()
+    
+    async def button_pressed():
+        await asyncio.sleep(0.2)
+        root.close()
+    
+    asyncio.ensure_future(button_pressed())
+    
+    root.mainloop()
--- tests/tk_mock.py
+++ tests/tk_mock.py
@@ -6,5 +6,6 @@ def create_tk_mock() -> Mock:
     tk_mock = Mock()
     updater = AsyncTkUpdater(tk_mock)
     tk_mock.mainloop = updater.mainloop
+    
     tk_mock.close = updater._on_close
     return tk_mock
