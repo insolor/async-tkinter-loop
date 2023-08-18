218b734885b9d52ce0c679cb34a3317c544e7056
insolor <insolor@gmail.com>
2023-08-18 19:21:10 +0300

    Experimental support of CustomTkinter using mixins

---
+++ async_tkinter_loop/mixins.py
@@ -0,0 +1,24 @@
+import sys
+import tkinter
+
+from async_tkinter_loop import async_mainloop
+
+
+class AsyncTk:
+    def mainloop(self: tkinter.Tk):
+        async_mainloop(self)
+
+
+class AsyncCTk(AsyncTk):
+    def mainloop(self):
+        if not self._window_exists:
+            if sys.platform.startswith("win"):
+                self._windows_set_titlebar_color(self._get_appearance_mode())
+
+                if not self._withdraw_called_before_window_exists and not self._iconify_called_before_window_exists:
+                    # print("window dont exists -> deiconify in mainloop")
+                    self.deiconify()
+
+            self._window_exists = True
+
+        super().mainloop()
