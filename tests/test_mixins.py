b58a7cc5a3282966564814fb9772ca371e5783a5
insolor <insolor@gmail.com>
2023-08-21 20:15:37 +0300

    Test mixins

---
+++ tests/test_mixins.py
@@ -0,0 +1,28 @@
+import tkinter
+
+import customtkinter
+import pytest
+
+from async_tkinter_loop.mixins import AsyncCTk, AsyncTk
+
+TIMEOUT = 60
+
+
+@pytest.mark.timeout(TIMEOUT)
+def test_destroy_tk():
+    class App(tkinter.Tk, AsyncTk):
+        pass
+
+    app = App()
+    app.destroy()
+    app.async_mainloop()
+
+
+@pytest.mark.timeout(TIMEOUT)
+def test_destroy_ctk():
+    class App(customtkinter.CTk, AsyncCTk):
+        pass
+
+    app = App()
+    app.destroy()
+    app.async_mainloop()
